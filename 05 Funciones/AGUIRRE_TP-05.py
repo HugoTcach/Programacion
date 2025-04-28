d3ebec0937857e50271d36194f4a66d897c72765
Cristian690 <cga_1985@hotmail.com>
2025-04-28 12:20:15 -0300

    Subo TP5 listas al repositorio en archivo .py

--- 05 Funciones/AGUIRRE_TP-05.py
+++ 05 Funciones/AGUIRRE_TP-05.py
@@ -117,14 +117,14 @@
 # Solicitar los números al usuario y mostrar el resultado usando esta
 # función.
 
-# def calcular_promedio(a ,b ,c):
-#     promedio = (a + b +c) / 3
-#     return promedio
+def calcular_promedio(a ,b ,c):
+     promedio = (a + b +c) / 3
+     return promedio
 
-# num1 = int(input("Ingrese el primer número: "))
-# num2 = int(input("Ingrese el segundo número: "))
-# num3 = int(input("Ingrese el tercer número: "))
+num1 = int(input("Ingrese el primer número: "))
+num2 = int(input("Ingrese el segundo número: "))
+num3 = int(input("Ingrese el tercer número: "))
 
-# promedio = calcular_promedio(num1, num2, num3)
+promedio = calcular_promedio(num1, num2, num3)
 
-# print("El promedio de los numeros ingresados es: ", round(promedio, 2))
+print("El promedio de los numeros ingresados es: ", round(promedio, 2))
